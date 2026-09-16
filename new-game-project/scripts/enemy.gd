7b218823195615647854613487573e7824725f75
Kyogoson <kysonmartinson@gmail.com>
2026-09-15 20:05:57 -0600

    hooray, more placeholders!

---
+++ new-game-project/scripts/enemy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+const SPEED = 30
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += direction * SPEED * delta
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
