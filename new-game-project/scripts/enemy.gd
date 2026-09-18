f63ccb07a67cb2bf7734185b5fc8806d02fae8a2
Kyogoson <kysonmartinson@gmail.com>
2026-09-17 20:29:58 -0600

    it turned into a straight platformer, but I'm having fun again, I'll get back on track once I know what I'm doing

--- new-game-project/scripts/enemy.gd
+++ new-game-project/scripts/enemy.gd
@@ -1,7 +1,10 @@
 extends Area2D
-
-const SPEED = 30
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var SPEED = 30
 var direction = 1
+var wait = 1
+
+signal collision
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,4 +16,19 @@ func _process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
+	wait = wait + 1
+	if wait <= 1:
+		SPEED = 30
+	elif wait == 4:
+		animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+		wait = 1
+		SPEED = 30
+		animated_sprite_2d.animation = "Run"
+	else:
+		animated_sprite_2d.animation = "Idle"
+		SPEED = 0
 	direction *= -1
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player character":
+		emit_signal("collision", body)
